b741b9d310bb19dde0767f6b69c504ccae05fa5e
Eric Ford <ebf11@psu.edu>
2021-05-21 02:19:13 -0400

    captions

--- baby.jl
+++ baby.jl
@@ -21,8 +21,10 @@ begin
        Pkg.activate(env)
        Pkg.Registry.update()
        Pkg.add(Pkg.PackageSpec(;name="PlutoUI", version="0.7"))
+	   Pkg.add(Pkg.PackageSpec(;name="HypertextLiteral", version="0.7"))
     end
     using PlutoUI
+	using HypertextLiteral
 end
 
 # ╔═╡ 395ad583-ff7a-4357-ab4d-cdc4eaa2a1a1
@@ -30,11 +32,22 @@ md"Code in cells below"
 
 # ╔═╡ 1a712530-882e-416f-9814-97d285e5ec8e
 begin
-	img_url_list = String[];
-	push!(img_url_list,"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Chin_posing.jpg/998px-Chin_posing.jpg")
-    push!(img_url_list,"https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg")
-    push!(img_url_list,"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Lengthy_body_Kanni.jpg/972px-Lengthy_body_Kanni.jpg")
+	img_url_list = Tuple{String,String}[];
+	push!(img_url_list,("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Chin_posing.jpg/998px-Chin_posing.jpg","Hello"))
+    push!(img_url_list,("https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg","World"))
+    push!(img_url_list,("https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Lengthy_body_Kanni.jpg/972px-Lengthy_body_Kanni.jpg","Hello Wordl"))
 end;
+
+# ╔═╡ d2e5fc28-908f-4fbb-926e-3be4d04301f0
+md"""
+Viewing angle $(@bind img_id Slider(1:length(img_url_list);default=1))
+"""
+
+# ╔═╡ cd389a3b-4299-4203-a3f2-1141ae1194d7
+@htl("<h2>$(img_url_list[img_id][2])</h2>")
+
+# ╔═╡ ae432659-98f8-40eb-8ae4-39298d297aff
+#res_list = map(s->Resource(s[1],  :width=>img_width, :height=>img_height),img_url_list);
 
 # ╔═╡ f4c4de78-9ead-442f-aa23-83cceec13544
 begin
@@ -42,17 +55,9 @@ begin
         img_height = "600"
 end;
 
-# ╔═╡ ae432659-98f8-40eb-8ae4-39298d297aff
-res_list = map(s->Resource(s,  :width=>img_width, :height=>img_height),img_url_list);
-
-# ╔═╡ d2e5fc28-908f-4fbb-926e-3be4d04301f0
+# ╔═╡ 74597c2b-f4d4-41b3-b826-d8a2d7bb6e45
 md"""
-Viewing angle $(@bind x Slider(1:length(res_list);default=1))
-"""
-
-# ╔═╡ db30fdba-7ff3-4f3d-8c76-ee7a41f60988
-md"""
-$(res_list[x])
+$(Resource(img_url_list[img_id][1], :height=>img_height, :width=>img_width))
 """
 
 # ╔═╡ 9d5a69ca-df63-4402-a48e-bc76571112f5
@@ -60,7 +65,9 @@ html"""
 <style>
 main {
 	max-width: 97% ! important;
-	margin-left: 10px !important;
+	margin-left: 20px !important;
+ 	margin-right: 20px !important;
+    align-self: flex-end;
 }
 header {
 	display: none;
@@ -76,7 +83,8 @@ footer {
 
 # ╔═╡ Cell order:
 # ╟─d2e5fc28-908f-4fbb-926e-3be4d04301f0
-# ╟─db30fdba-7ff3-4f3d-8c76-ee7a41f60988
+# ╟─74597c2b-f4d4-41b3-b826-d8a2d7bb6e45
+# ╟─cd389a3b-4299-4203-a3f2-1141ae1194d7
 # ╟─395ad583-ff7a-4357-ab4d-cdc4eaa2a1a1
 # ╟─1a712530-882e-416f-9814-97d285e5ec8e
 # ╟─ae432659-98f8-40eb-8ae4-39298d297aff
