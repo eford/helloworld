c27d51b276bd960d43c97e679981c84098e528d1
Eric Ford <ebf11@psu.edu>
2021-05-21 01:13:42 -0400

    cleanup UI

--- baby.jl
+++ baby.jl
@@ -13,76 +13,67 @@ macro bind(def, element)
     end
 end
 
-# ╔═╡ ae0936ee-c767-11ea-0cbc-3f58779113da
+# ╔═╡ aa52e565-c603-4adc-ae2a-a21da99f8865
 begin
 	let
-		env = mktempdir()
-		import Pkg
-		Pkg.activate(env)
-		Pkg.Registry.update()
-		Pkg.add(Pkg.PackageSpec(;name="PlutoUI", version="0.7"))
-	end
-	using PlutoUI
-end;
+       env = mktempdir()
+       import Pkg
+       Pkg.activate(env)
+       Pkg.Registry.update()
+       Pkg.add(Pkg.PackageSpec(;name="PlutoUI", version="0.7"))
+    end
+    using PlutoUI
+end
 
-# ╔═╡ 3eff9592-cc63-11ea-2b61-4170d1a7656a
-md"# Setup code"
+# ╔═╡ 395ad583-ff7a-4357-ab4d-cdc4eaa2a1a1
+md"Code in cells below"
 
-# ╔═╡ cb47403b-13f8-4fe6-8ad3-b125ebdfb88e
+# ╔═╡ 1a712530-882e-416f-9814-97d285e5ec8e
 begin
-	img_url1 = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Chin_posing.jpg/998px-Chin_posing.jpg"
-	img_url2 = "https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg"
-	img_url3 = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Lengthy_body_Kanni.jpg/972px-Lengthy_body_Kanni.jpg"
+	img_url_list = String[];
+	push!(img_url_list,"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Chin_posing.jpg/998px-Chin_posing.jpg")
+    push!(img_url_list,"https://upload.wikimedia.org/wikipedia/commons/9/99/Brooks_Chase_Ranger_of_Jolly_Dogs_Jack_Russell.jpg")
+    push!(img_url_list,"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Lengthy_body_Kanni.jpg/972px-Lengthy_body_Kanni.jpg")
 end;
 
-# ╔═╡ 223cc898-c35c-4216-a1af-ce43bad7ed51
+# ╔═╡ f4c4de78-9ead-442f-aa23-83cceec13544
 begin
-	img_width = 800
-	img_height = 600
+        img_width = "auto" 
+        img_height = "600"
 end;
 
-# ╔═╡ 1004db48-8715-4aca-ba21-a6e7484a5753
-res_list = [Resource(img_url1, :width=>img_width,:height=>img_height), Resource(img_url2,:width=>img_width,:height=>img_height), Resource(img_url3,:width=>img_width,:height=>img_height)];
+# ╔═╡ ae432659-98f8-40eb-8ae4-39298d297aff
+res_list = map(s->Resource(s,  :width=>img_width, :height=>img_height),img_url_list);
 
-# ╔═╡ 34ebf81e-c760-11ea-05bb-376173e7ed10
-@bind x Slider(1:length(res_list);default=1)
+# ╔═╡ d2e5fc28-908f-4fbb-926e-3be4d04301f0
+md"""
+Viewing angle $(@bind x Slider(1:length(res_list);default=1))
+"""
 
-# ╔═╡ cdabd9a5-bde2-4552-a4de-162f4a412ca4
+# ╔═╡ db30fdba-7ff3-4f3d-8c76-ee7a41f60988
 md"""
 $(res_list[x])
 """
 
-# ╔═╡ 09623ed1-966d-46fc-8031-203eba226381
+# ╔═╡ 9d5a69ca-df63-4402-a48e-bc76571112f5
 html"""
 <style>
-body.hide_all_inputs pluto-input {
-	display: none;
+main {max-width: 100vw;}
+header {
+display: none;
 }
-body.hide_all_inputs pluto-shoulder {
-	display: none;
-}
-body.hide_all_inputs pluto-trafficlight {
-	display: none;
-}
-body.hide_all_inputs pluto-runarea {
-	display: none;
-}
-body.hide_all_inputs .add_cell {
-	display: none;
-}
-body.hide_all_inputs pluto-cell {
-	min-height: 0;
-	margin-top: 10px;
+footer {
+display: none;
 }
 </style>
 """
 
 # ╔═╡ Cell order:
-# ╟─cdabd9a5-bde2-4552-a4de-162f4a412ca4
-# ╟─34ebf81e-c760-11ea-05bb-376173e7ed10
-# ╟─3eff9592-cc63-11ea-2b61-4170d1a7656a
-# ╟─ae0936ee-c767-11ea-0cbc-3f58779113da
-# ╠═cb47403b-13f8-4fe6-8ad3-b125ebdfb88e
-# ╠═223cc898-c35c-4216-a1af-ce43bad7ed51
-# ╠═1004db48-8715-4aca-ba21-a6e7484a5753
-# ╠═09623ed1-966d-46fc-8031-203eba226381
+# ╟─d2e5fc28-908f-4fbb-926e-3be4d04301f0
+# ╟─db30fdba-7ff3-4f3d-8c76-ee7a41f60988
+# ╟─395ad583-ff7a-4357-ab4d-cdc4eaa2a1a1
+# ╟─1a712530-882e-416f-9814-97d285e5ec8e
+# ╟─ae432659-98f8-40eb-8ae4-39298d297aff
+# ╟─f4c4de78-9ead-442f-aa23-83cceec13544
+# ╟─9d5a69ca-df63-4402-a48e-bc76571112f5
+# ╟─aa52e565-c603-4adc-ae2a-a21da99f8865
